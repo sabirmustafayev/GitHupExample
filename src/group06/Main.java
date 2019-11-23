f620d76b6d64a5ea20d189426a7a76eb453a0759
sabir mustafayev <sabirmustafayev86@gmail.com>
2019-11-23 11:28:06 -0500

    src folder added

---
+++ src/group06/Main.java
@@ -0,0 +1,11 @@
+package group06;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 10;
+     int b = 10;
+      int total = a + b;
+        System.out.println(total);
+}
+}
