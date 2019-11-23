456e6804d84a1d60c9864408a4af54aa8f15a195
sabir mustafayev <sabirmustafayev86@gmail.com>
2019-11-23 11:47:51 -0500

    String variable added to the class

--- src/group06/online06.java
+++ src/group06/online06.java
@@ -1,11 +1,15 @@
 package group06;
 
-public class Main {
+public class online06 {
 
     public static void main(String[] args) {
 	int a = 10;
      int b = 10;
       int total = a + b;
         System.out.println(total);
+
+        String name = "kartal";
+        System.out.println(name);
 }
+
 }
